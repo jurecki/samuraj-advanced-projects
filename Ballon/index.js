d9046594465a43cb2c7a579258f39cd342f403f0
Pawel Jurecki <jurecki.pawel@gmail.com>
2020-10-21 12:48:19 +0200

    Create AnimBg class and use it in the project

--- Ballon/index.js
+++ Ballon/index.js
@@ -1,22 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const elements = document.querySelectorAll('.anim-bg')
 
-    document.addEventListener('mousemove', function(event) {
-        const { clientX, clientY } = event;
-        
-        const centerX =window.innerWidth/2;
-        const centerY = window.innerHeight/2;
+    const animBg = new AnimBg('.anim-bg');
 
-        elements.forEach(element => {
-            const ratioX = -element.getAttribute('ratioX');
-            const ratioY = -element.getAttribute('ratioY');
-
-            const moveX = clientX - centerX;
-            const moveY = clientY - centerY;
-
-            element.style.transform = `translate(${moveX*ratioX}px, ${moveY*ratioY}px)`
-        })
-
-    })
+   
+    document.addEventListener('mousemove', (e) => animBg.listenCursorMove(e))
 
 })
