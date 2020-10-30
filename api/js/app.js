796881274dc1823cb61f88cb962b26a4388edd26
Pawel Jurecki <jurecki.pawel@gmail.com>
2020-10-30 12:24:27 +0100

    API project done

--- api/js/app.js
+++ api/js/app.js
@@ -2,14 +2,32 @@ import '../sass/style.scss';
 
 class Dogo {
     constructor() {
-         this.apiUrl = "https://dog.ceo/api/breeds/";
+         this.apiUrl = "https://dog.ceo/api";
          this.imgEl = document.querySelector('.featured-dog img')
          this.backgroundEl =  document.querySelector('.featured-dog__background')
+         this.tilesEl = document.querySelector('.tiles');
+         this.spinnerEl = document.querySelector('.spinner');
          this.init()
     }
 
-    listBreads() {
-      return  fetch(`${this.apiUrl}list/all`)
+    showLoading() {
+        this.spinnerEl.classList.add('spinner--visible')
+    }
+
+    hideLoading() {
+        this.spinnerEl.classList.remove('spinner--visible')
+    }
+
+    showImageWhenReady(src) {
+      
+        this.imgEl.setAttribute('src', src);
+        this.backgroundEl.style.background=`url("${src}")`
+        this.hideLoading()
+   
+}
+
+    listBreeds() {
+      return  fetch(`${this.apiUrl}/breeds/list/all`)
                  .then(resp => resp.json())
                  .then(data => data.message)
                  .catch(err => console.log(err))
@@ -17,27 +35,72 @@ class Dogo {
     
     
     getRandomImage() {
-       return  fetch(`${this.apiUrl}image/random`)
+       return  fetch(`${this.apiUrl}/breeds/image/random`)
                  .then(resp => resp.json())
                  .then(data => data.message)
                  .catch(err=> console.log(err))
     }
     
     getRandomImageByBreed(breed) {
-        return  fetch(`${this.apiUrl}${breed}/images`)
+        return  fetch(`${this.apiUrl}/breed/${breed}/images/random`)
                     .then(resp => resp.json())
                     .then(data => data.message)
     }
 
-    init() {
-        this.getRandomImage()
-            .then(src => {
-                this.imgEl.setAttribute('src', src);
-                this.backgroundEl.style.backgroundImage = `url("${src}")`
+    showAllBreeds() {
+        this.listBreeds()
+            .then(breeds => {
+                for(const breed in breeds) {
+                    if(breeds[breed].length==0) {
+                        this.addBreed(breed)
+
+                    } else {
+                        for(const subBreed of breeds[breed]) {
+                           this.addBreed(breed, subBreed)
+                        }
+                    }
+                   
+                }
             })
+    }
+
+    addBreed(breed, subBreed) {
+        let name;
+        let type;
+        
+        if(typeof subBreed === 'undefined') {
+            name = breed;
+            type = breed;
+        } else {
+            name = `${breed} ${subBreed}`;
+            type = `${breed}`
+        } 
+
+        const tile = document.createElement('div');
+        tile.classList.add('tiles__tile');
+        const titleContent = document.createElement('div');
+        titleContent.classList.add('tiles__tile-content');
+
+        titleContent.innerText = name;
+        titleContent.addEventListener('click', ()=> {
+            window.scrollTo(0,0)
+            this.showLoading()
+            this.getRandomImageByBreed(type)
+            .then((img) => this.showImageWhenReady(img))
+        })
+
+        tile.appendChild(titleContent);
+        this.tilesEl.appendChild(tile)
+    }
+
+
+
+    init() {
+        this.showLoading();
+        this.getRandomImage()
+            .then((img)=> this.showImageWhenReady(img))
        
-        this.listBreads()
-            .then(data => console.log('data', data))
+        this.showAllBreeds();
     }   
 }
 
